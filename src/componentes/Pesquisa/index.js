3fed24af1cd248b58ad4601e4f3a238fc02d59f3
Nahyron <nahyron.jesus@aluno.senai.br>
2025-10-14 17:31:51 -0300

    meu hover não funciona

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
     backgorund-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -9,34 +10,65 @@ const PesquisaContainer = styled.section`
     padding: 85px 0;
     height: 270px;
     width: 100%;
-`
+`;
 
 const Titulo = styled.h2`
  color: #FFF;
  font-size: 36px;
  text-align: center;
- width: 100%;
+ dthtrtrwidth: 100%;
 
-`
+`;
 
 const Subtitulo = styled.h3`
 font-size: 16px;
 font-weight: 500px;
 margin-bottom: 40px;
 
-`
+`;
+
+const Resultado = styled.div `
+display:flex;
+justify-content: center;
+align-items: center;
+margin-bottom: 20px;
+cursor: pointer;
+p{
+    width: 200px;
+}
+
+img{
+    width: 100px;
+}
+
+&:hover{
+border 1px solid white;
+}
+`;
 
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('');
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+    console.log(livrosPesquisados)
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura:"
-                onBlur={evento => setTextoDigitado (evento.target.value)}
+                onInput={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(livros => livros.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
-            <p>{textoDigitado}</p>
+
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src} alt="oi"/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            ))}
         </PesquisaContainer>
     );
 }
